d6e6a80b85ba71c6aa6ade10c8d72ed8efc4783a
JamesPropPitt <propthevampire@gmail.com>
2017-10-02 16:02:13 +0100

    errors tho

---
+++ COMP220/COMP220_Examples/1_OpenGL/main.cpp
@@ -0,0 +1,94 @@
+//main.cpp - defines the entry point of the application
+
+#include "main.h"
+
+int main(int argc, char* args[])
+{
+	//Initialises the SDL Library, passing in SDL_INIT_VIDEO to only initialise the video subsystems
+	//https://wiki.libsdl.org/SDL_Init
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		//Display an error message box
+		//https://wiki.libsdl.org/SDL_ShowSimpleMessageBox
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, SDL_GetError(), "SDL_Init failed", NULL);
+		return 1;
+	}
+
+	//Create a window, note we have to free the pointer returned using the DestroyWindow Function
+	//https://wiki.libsdl.org/SDL_CreateWindow
+	SDL_Window* window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN|SDL_WINDOW_OPENGL );
+	//Checks to see if the window has been created, the pointer will have a value of some kind
+	if (window == nullptr)
+	{
+		//Show error
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, SDL_GetError(), "SDL_CreateWindow failed", NULL);
+		//Close the SDL Library
+		//https://wiki.libsdl.org/SDL_Quit
+		SDL_Quit();
+		return 1;
+	}
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (glewInit() != GLEW_OK) {
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,"OPENGL DID NOT WAKE UP",(char*)glewGetErrorString(err), NULL);
+	}
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+	SDL_GLContext gl_Context = SDL_GL_CreateContext(window);
+	if (gl_Context == nullptr) {
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "OPENGL DID NOT WAKE UP", (char*)glewGetErrorString(err), NULL);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+
+		return 1;
+	}
+
+	//Event loop, we will loop until running is set to false, usually if escape has been pressed or window is closed
+	bool running = true;
+	//SDL Event structure, this will be checked in the while loop
+	SDL_Event ev;
+	while (running)
+	{
+		//Poll for the events which have happened in this frame
+		//https://wiki.libsdl.org/SDL_PollEvent
+		while (SDL_PollEvent(&ev))
+		{
+			//Switch case for every message we are intereted in
+			switch (ev.type)
+			{
+				//QUIT Message, usually called when the window has been closed
+			case SDL_QUIT:
+				running = false;
+				break;
+				//KEYDOWN Message, called when a key has been pressed down
+			case SDL_KEYDOWN:
+				//Check the actual key code of the key that has been pressed
+				switch (ev.key.keysym.sym)
+				{
+					//Escape key
+				case SDLK_ESCAPE:
+					running = false;
+					break;
+				}
+			}
+		}
+		glClearColor(0.0, 0.0, 0.0, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		SDL_GL_SwapWindow(window);
+
+
+		
+	}
+
+	SDL_GL_DeleteContext(gl_Context);
+	//Destroy the window and quit SDL2, NB we should do this after all cleanup in this order!!!
+	//https://wiki.libsdl.org/SDL_DestroyWindow
+	SDL_DestroyWindow(window);
+	//https://wiki.libsdl.org/SDL_Quit
+	SDL_Quit();
+
+	return 0;
+}
